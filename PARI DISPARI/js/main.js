757b95059b1104ef2a3ab54a8f0e6a75d387eac1
Yu4iy <yurimohyliuk@gmail.com>
2021-11-01 11:05:03 +0100

    add Pari e Dispari v.0.0.2

--- PARI DISPARI/js/main.js
+++ PARI DISPARI/js/main.js
@@ -5,38 +5,62 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+// Pari e Dispari v.0.0.2
+const btn = document.querySelector('.btn__send')
 
-const parDis = prompt('inserire pari o dispari')
-const userNumber = parseInt(prompt('inserire numero da 1 a 5'))
-const computerNumber = randomNum(1 , 5)
-console.log(computerNumber)
+btn.addEventListener('click', function(){
+	let parDis = document.querySelector('.input__pari-dispari').value
+	
+	let userNumber = parseInt(document.querySelector('.input__number').value)
+	console.log(userNumber)
 
-
-let somma = computerNumber + userNumber
-console.log(somma)
-let pariOrDispari = pariDispari(somma)
-
-if (pariOrDispari === parDis){
-	alert('hai vinto')
-}
-else{
-	alert('hai perso')
-}
+	const computerNumber = randomNum(1 , 5)
+	console.log(computerNumber)
 
 
 
+	while((parDis !== 'pari') && (parDis !== 'dispari')){
+		alert('Inserire PARI O DISPARI')
+	}
 
-function pariDispari(number) { 
-	if(number % 2 === 0){
-		return 'pari';
+	while(userNumber > 5 || userNumber < 0 || isNaN(userNumber)){
+		alert('Inserire numero da 1 a 5')
+	}
+
+
+
+	const somma = computerNumber + userNumber
+	console.log(somma)
+
+	let pariOrDispari = pariDispari(somma)
+
+	const resultText = document.querySelector('.main-screen__result')
+	const resultNum = document.querySelector('.main-screen__number')
+	
+	if (pariOrDispari === parDis){
+		resultText.innerHTML = 'HAI VINTO'
+		resultNum.innerHTML = somma
 	}
 	else{
-		return 'dispari'
+		resultText.innerHTML = 'HAI PERSO'
+		resultNum.innerHTML = somma
+
 	}
-}
+
+})
 
 
-function randomNum(min , max) {
-	const rand = Math.floor(Math.random() * (max - min + 1)) + min;
-	return rand;
-}
+	function pariDispari(number) { 
+		if(number % 2 === 0){
+			return 'pari';
+		}
+		else{
+			return 'dispari'
+		}
+	}
+
+
+	function randomNum(min , max) {
+		const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+		return rand;
+	}
