45193d3d29cc78de8c491197a2867edd9089ffbe
Yu4iy <yurimohyliuk@gmail.com>
2021-11-01 12:02:25 +0100

    add  LowerCase, trim controle

--- PARI DISPARI/js/main.js
+++ PARI DISPARI/js/main.js
@@ -9,58 +9,55 @@
 const btn = document.querySelector('.btn__send')
 
 btn.addEventListener('click', function(){
-	let parDis = document.querySelector('.input__pari-dispari').value
-	
+	let parDis = document.querySelector('.input__pari-dispari').value.toLowerCase().trim()
+
 	let userNumber = parseInt(document.querySelector('.input__number').value)
 	console.log(userNumber)
 
 	const computerNumber = randomNum(1 , 5)
 	console.log(computerNumber)
 
-
-
-	while((parDis !== 'pari') && (parDis !== 'dispari')){
-		alert('Inserire PARI O DISPARI')
-	}
-
-	while(userNumber > 5 || userNumber < 0 || isNaN(userNumber)){
-		alert('Inserire numero da 1 a 5')
-	}
-
-
-
-	const somma = computerNumber + userNumber
-	console.log(somma)
-
-	let pariOrDispari = pariDispari(somma)
-
-	const resultText = document.querySelector('.main-screen__result')
-	const resultNum = document.querySelector('.main-screen__number')
+	if(((parDis !== 'pari') && (parDis !== 'dispari')) || (userNumber > 5 || userNumber < 0 || isNaN(userNumber))){
+	alert('INSERIRE DATI CORETTI')
+	}else{
+		const somma = computerNumber + userNumber
+		console.log(somma)
 	
-	if (pariOrDispari === parDis){
-		resultText.innerHTML = 'HAI VINTO'
-		resultNum.innerHTML = somma
+		let pariOrDispari = pariDispari(somma)
+	
+		const resultText = document.querySelector('.main-screen__result')
+		const resultNum = document.querySelector('.main-screen__number')
+		
+		if (pariOrDispari === parDis){
+			resultText.innerHTML = 'HAI VINTO'
+			resultNum.innerHTML = somma
+		}
+		else{
+			resultText.innerHTML = 'HAI PERSO'
+			resultNum.innerHTML = somma
+	
+		}
 	}
-	else{
-		resultText.innerHTML = 'HAI PERSO'
-		resultNum.innerHTML = somma
-
-	}
-
+	
+	
+	
+	
+	
 })
 
 
-	function pariDispari(number) { 
-		if(number % 2 === 0){
-			return 'pari';
-		}
-		else{
-			return 'dispari'
-		}
+function pariDispari(number) { 
+	if(number % 2 === 0){
+		return 'pari';
 	}
+	else{
+		return 'dispari'
+	}
+}
 
 
-	function randomNum(min , max) {
-		const rand = Math.floor(Math.random() * (max - min + 1)) + min;
-		return rand;
-	}
+function randomNum(min , max) {
+	const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+	return rand;
+}
+
